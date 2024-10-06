b8fd3a46f3bd6943f7c506ba7facd110e44df0c5
gbcostasant@gmail.com <gbcostasant@gmail.com>
2024-10-06 16:21:00 -0300

    update header

--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -1,17 +1,34 @@
 import { Link } from "react-router-dom";
+import bwLogo from "../assets/bw2.png";
+const ButtonMenu = ({ to, children }: { to: string; children: string }) => {
+  return (
+    <Link to={to}>
+      <span className="hover:text-indigo-500 transition-colors duration-300 ease-in-out">
+        {children}
+      </span>
+    </Link>
+  );
+};
 const Menu = () => {
   return (
     <div
-      className="flex flex-row p-2 font-bold text-white w-screen px-12
-            items-center shadow-md fixed z-40 bg-blue-950 top-0 left-1/2 -translate-x-1/2"
+      className="flex flex-row font-bold text-white w-screen px-24 max-h-36 py-3
+            items-center shadow-md fixed z-40 bg-slate-900 justify-between"
     >
-      <Link to="/thirdStrike" className="ml-4 cursor-pointer text-2xl">
-        BW
-      </Link>
-      <div className="flex flex-row ml-16 font-bold cursor-pointer gap-6 text-lg">
-        <Link to={"/thirdStrike/guides"}>Guides</Link>
-        <Link to={"/thirdStrike/recursos"}>Recursos</Link>
-        <Link to={"/thirdStrike/matchups"}>Matchups</Link>
+      <div>
+        <Link
+          to="/thirdStrike"
+          className="ml-4 cursor-pointer text-2xl flex flex-row gap-6 items-center"
+        >
+          <img src={bwLogo} className="w-24" />
+          <span className="text-4xl text-indigo-600">Blue Wolves</span>
+        </Link>
+      </div>
+
+      <div className="flex flex-row ml-16 font-bold cursor-pointer gap-6 text-xl">
+        <ButtonMenu to={"/thirdStrike/guides"}>GUIDES</ButtonMenu>
+        <ButtonMenu to={"/thirdStrike/recursos"}>RECURSOS</ButtonMenu>
+        <ButtonMenu to={"/thirdStrike/matchups"}>MATCHUPS</ButtonMenu>
       </div>
     </div>
   );
