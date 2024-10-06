0d6d32e1d4a0ecefd503aaa1229512315ef2ae5b
gbcostasant@gmail.com <gbcostasant@gmail.com>
2024-10-06 19:01:10 -0300

    add font

--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -13,7 +13,7 @@ const Menu = () => {
   return (
     <div
       className="flex flex-row font-bold text-white w-screen pl-72 pr-56 max-h-36 py-3
-            items-center shadow-md fixed z-40 bg-[#0b0b3d] justify-between"
+            items-center shadow-md fixed z-40 bg-[#0b0b3d] justify-between font-pRegular"
     >
       <div>
         <Link
@@ -21,11 +21,11 @@ const Menu = () => {
           className="ml-4 cursor-pointer text-2xl flex flex-row gap-6 items-center"
         >
           <img src={bwLogo} className="w-20" />
-          <span className="text-5xl text-[#3B3FE9]">BLUE WOLVES</span>
+          <span className="text-[6pt] text-[#3B3FE9]">BLUE WOLVES</span>
         </Link>
       </div>
 
-      <div className="flex flex-row ml-16 font-bold cursor-pointer gap-12 text-3xl">
+      <div className="flex flex-row ml-16 font-bold cursor-pointer gap-12 text-[3.5pt]">
         <ButtonMenu to={"/thirdStrike/guides"}>GUIDES</ButtonMenu>
         <ButtonMenu to={"/thirdStrike/recursos"}>RECURSOS</ButtonMenu>
         <ButtonMenu to={"/thirdStrike/matchups"}>MATCHUPS</ButtonMenu>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      fontFamily: {
+        pRegular: ["regular"],
+      },
       spacing: {
         alexLeft: "398px",
         alexTop: "282px",
