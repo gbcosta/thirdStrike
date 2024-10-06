5f22b54bcc6b5ef09833bf16d8343c1003ad908c
gbcostasant@gmail.com <gbcostasant@gmail.com>
2024-10-06 18:32:23 -0300

    update header

--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -12,7 +12,7 @@ const ButtonMenu = ({ to, children }: { to: string; children: string }) => {
 const Menu = () => {
   return (
     <div
-      className="flex flex-row font-bold text-white w-screen pl-48 pr-16 max-h-36 py-3
+      className="flex flex-row font-bold text-white w-screen pl-72 pr-56 max-h-36 py-3
             items-center shadow-md fixed z-40 bg-[#0b0b3d] justify-between"
     >
       <div>
@@ -21,11 +21,11 @@ const Menu = () => {
           className="ml-4 cursor-pointer text-2xl flex flex-row gap-6 items-center"
         >
           <img src={bwLogo} className="w-20" />
-          <span className="text-4xl text-[#3B3FE9]">BLUE WOLVES</span>
+          <span className="text-5xl text-[#3B3FE9]">BLUE WOLVES</span>
         </Link>
       </div>
 
-      <div className="flex flex-row ml-16 font-bold cursor-pointer gap-12 text-2xl">
+      <div className="flex flex-row ml-16 font-bold cursor-pointer gap-12 text-3xl">
         <ButtonMenu to={"/thirdStrike/guides"}>GUIDES</ButtonMenu>
         <ButtonMenu to={"/thirdStrike/recursos"}>RECURSOS</ButtonMenu>
         <ButtonMenu to={"/thirdStrike/matchups"}>MATCHUPS</ButtonMenu>
